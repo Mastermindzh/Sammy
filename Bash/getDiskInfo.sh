a41e4ba5990602a0444de9e5abf55e77ff932082
jancoow <jancokock@gmail.com>
2016-07-17 19:42:48 +0200

    Reading disks and partitions from disk

--- Bash/getDiskInfo.sh
+++ Bash/getDiskInfo.sh
@@ -7,46 +7,56 @@
 # include json functions
 . "$(dirname "$0")/"Includes/jsonFunctions.sh 
 
+# declare some other functions
+getPartitionInformation() {
+	local returnjson=''
+	currentdevice=$1
+	while read line; do 
+		if [[ $line == *"part"*  ]] && [[ $line == *"$currentdevice"*  ]]
+		then
+			#Get disk usage information about this partition
+			declare -A PartitionInformation
+			currentpartition=$(echo $line | awk '{print $1}')
+			PartitionInformation["type"]=$(echo "$diskspace" | grep $currentpartition | awk '{print $2}')
+			PartitionInformation["totalsize"]=$(echo "$diskspace" | grep $currentpartition | awk '{print $3}')
+			PartitionInformation["usedsize"]=$(echo "$diskspace" | grep $currentpartition | awk '{print $4}')
+			PartitionInformation["availablesize"]=$(echo "$diskspace" | grep $currentpartition | awk '{print $5}')
+			PartitionInformation["mountpoint"]=$(echo "$diskspace" | grep $currentpartition | awk '{print $7}')
+			
+			#convert this to json 
+			json="$(getJson "$(declare -p PartitionInformation)" $currentpartition)"
+			
+			#Add this information to the total json object
+			returnjson=$returnjson$json","
+		fi
+	done < <(lsblk -ln)
+	
+	#remove last ','
+	returnjson="${returnjson::-1}"
+	
+	#finish up json
+	returnjson="\"partitions\":{$returnjson}"
+	echo $returnjson
+}
+
 # read disk space
 diskspace=$(df -T -l)
 
-# first list all the disks connected to the device
+# loop through disks connected to the device
 declare -A DiskInfo
-currentdevice=""
 while read line; do 
-	if [[ $line == *"disk"* ]] #Set this as the current disk
+	if [[ $line == *"disk"* ]] #Disk found
 	then
-		if [[ $currentdevice != "" ]] 
-		then
-			DiskInfo[$currentdevice] = ${DiskInfo[$currentdevice]}"}"
-		fi
 		currentdevice=$(echo $line | awk '{print $1}')
-		DiskInfo[$currentdevice]="\"partitions\":{"
-	elif [[ $line == *"part"* ]] #Get information about this partition and add it to the disk information
-	then
-		declare -A PartitionInformation
-		currentpartition=$(echo $line | awk '{print $1}')
-		PartitionInformation["type"]=$(echo "$diskspace" | grep $currentpartition | awk '{print $2}')
-		PartitionInformation["totalsize"]=$(echo "$diskspace" | grep $currentpartition | awk '{print $3}')
-		PartitionInformation["usedsize"]=$(echo "$diskspace" | grep $currentpartition | awk '{print $4}')
-		PartitionInformation["availablesize"]=$(echo "$diskspace" | grep $currentpartition | awk '{print $5}')
-		PartitionInformation["mountpoint"]=$(echo "$diskspace" | grep $currentpartition | awk '{print $7}')
-		json="$(getJson "$(declare -p PartitionInformation)" $currentpartition)"
-		if [[ ${DiskInfo[$currentdevice]} != "\"partitions\":{" ]]
-		then
-			DiskInfo[$currentdevice]=${DiskInfo[$currentdevice]}","$json
-		else
-			DiskInfo[$currentdevice]=${DiskInfo[$currentdevice]}$json
-		fi
+		DiskInfo[$currentdevice]="\"$currentdevice\": {"
+		
+		#partition information
+		DiskInfo[$currentdevice]=${DiskInfo[$currentdevice]}$(getPartitionInformation $currentdevice)
+		#smartdata
+		#finish object
+		DiskInfo[$currentdevice]=${DiskInfo[$currentdevice]}"}"
 	fi
 done < <(lsblk -ln)
 
-if [[ $currentdevice != "" ]] 
-then
-	DiskInfo[$currentdevice]=${DiskInfo[$currentdevice]}"}"
-fi
-
-
 json="$(combineJson "$(declare -p DiskInfo)")"
 echo $json
- 
