e786c0030998e22b4fa9f819fe945c4b33f66527
jancoow <jancokock@gmail.com>
2016-07-17 19:42:48 +0200

    Reading disks and partitions from disk

--- Bash/getDiskInfo.sh
+++ Bash/getDiskInfo.sh
@@ -13,27 +13,40 @@ diskspace=$(df -T -l)
 # first list all the disks connected to the device
 declare -A DiskInfo
 currentdevice=""
-lsblk -ln | while read -r line; do 
+while read line; do 
 	if [[ $line == *"disk"* ]] #Set this as the current disk
 	then
+		if [[ $currentdevice != "" ]] 
+		then
+			DiskInfo[$currentdevice] = ${DiskInfo[$currentdevice]}"}"
+		fi
 		currentdevice=$(echo $line | awk '{print $1}')
-		DiskInfo[$currentdevice]=""
+		DiskInfo[$currentdevice]="\"partitions\":{"
 	elif [[ $line == *"part"* ]] #Get information about this partition and add it to the disk information
 	then
 		declare -A PartitionInformation
 		currentpartition=$(echo $line | awk '{print $1}')
-		PartitionInformation["filesystem"]=$currentpartition
 		PartitionInformation["type"]=$(echo "$diskspace" | grep $currentpartition | awk '{print $2}')
 		PartitionInformation["totalsize"]=$(echo "$diskspace" | grep $currentpartition | awk '{print $3}')
 		PartitionInformation["usedsize"]=$(echo "$diskspace" | grep $currentpartition | awk '{print $4}')
 		PartitionInformation["availablesize"]=$(echo "$diskspace" | grep $currentpartition | awk '{print $5}')
 		PartitionInformation["mountpoint"]=$(echo "$diskspace" | grep $currentpartition | awk '{print $7}')
-		json="$(getJson "$(declare -p PartitionInformation)" Partitions)"
-		DiskInfo[$currentdevice]=$json
+		json="$(getJson "$(declare -p PartitionInformation)" $currentpartition)"
+		if [[ ${DiskInfo[$currentdevice]} != "\"partitions\":{" ]]
+		then
+			DiskInfo[$currentdevice]=${DiskInfo[$currentdevice]}","$json
+		else
+			DiskInfo[$currentdevice]=${DiskInfo[$currentdevice]}$json
+		fi
 	fi
-done
+done < <(lsblk -ln)
+
+if [[ $currentdevice != "" ]] 
+then
+	DiskInfo[$currentdevice]=${DiskInfo[$currentdevice]}"}"
+fi
 
 
-json="$(getJson "$(declare -p DiskInfo)" DiskInfo)"
+json="$(combineJson "$(declare -p DiskInfo)")"
 echo $json
  
