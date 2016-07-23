4a907ad0a6fc91319c0fcdcdb60455b5aeddb693
jancoow <jancokock@gmail.com>
2016-07-23 23:52:07 +0200

    Added flask route

--- Sammy.py
+++ Sammy.py
@@ -20,4 +20,9 @@ def get_cpu_info():
 @app.route('/mem')
 def get_mem_info():
     process = subprocess.Popen("./Bash/getMemInfo.sh", stdout=subprocess.PIPE)
+    return process.communicate()[0]@app.route('/mem')
+
+@app.route('/disk')
+def get_disk_info():
+    process = subprocess.Popen("./Bash/getDiskInfo.sh", stdout=subprocess.PIPE)
     return process.communicate()[0]
