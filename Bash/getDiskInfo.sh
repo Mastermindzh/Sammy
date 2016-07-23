6db016d9839164d0ffde452011c134100b05ed0e
jancoow <jancokock@gmail.com>
2016-07-23 22:58:37 +0200

    Checked if dependecies are installed. Renamed variables.

--- Bash/getDiskInfo.sh
+++ Bash/getDiskInfo.sh
@@ -3,10 +3,11 @@ LANGUAGE=en
 # Author: Janco Kock
 # Contributors: Rick van Lieshout
 # Last modification date: 2016-07-16
-# dependencies: gsmartcontro, hddtemp
+# dependencies: gsmartcontrol, hddtemp
 
-# include json functions
+# include functions
 . "$(dirname "$0")/"Includes/json_functions.sh
+. "$(dirname "$0")/"Includes/functions.sh
 
 # vars
 diskspace=$(df -T -l)
@@ -14,60 +15,65 @@ disklist=$(lsblk -ln)
 
 # functions
 get_partition_information() {
-	local partitionJson
+	local partition_json
 	currentdevice=$1
 	while read line; do 
 		if [[ $line == *"part"*  ]] && [[ $line == *"$currentdevice"*  ]]
 		then
 			#Get disk usage information about this partition
-			declare -A PartitionInformation
+			declare -A partition_information
 			currentpartition=$(echo $line | awk '{print $1}')
-			PartitionInformation["type"]=$(echo "$diskspace" | grep $currentpartition | awk '{print $2}')
-			PartitionInformation["totalsize"]=$(echo "$diskspace" | grep $currentpartition | awk '{print $3}')
-			PartitionInformation["usedsize"]=$(echo "$diskspace" | grep $currentpartition | awk '{print $4}')
-			PartitionInformation["availablesize"]=$(echo "$diskspace" | grep $currentpartition | awk '{print $5}')
-			PartitionInformation["mountpoint"]=$(echo "$diskspace" | grep $currentpartition | awk '{print $7}')
+			partition_information["type"]=$(echo "$diskspace" | grep $currentpartition | awk '{print $2}')
+			partition_information["totalsize"]=$(echo "$diskspace" | grep $currentpartition | awk '{print $3}')
+			partition_information["usedsize"]=$(echo "$diskspace" | grep $currentpartition | awk '{print $4}')
+			partition_information["availablesize"]=$(echo "$diskspace" | grep $currentpartition | awk '{print $5}')
+			partition_information["mountpoint"]=$(echo "$diskspace" | grep $currentpartition | awk '{print $7}')
 			
 			#convert this to json 
-			json="$(get_json "$(declare -p PartitionInformation)" $currentpartition)"
+			json="$(get_json "$(declare -p partition_information)" $currentpartition)"
 			
 			#Combine this to the total json object, if the total json object is not empty
-			if [[ "$partitionJson" != "" ]]; then
-				array=("$json" "$partitionJson")
-				partitionJson=$(combine_json "${array[@]}")
+			if [[ "$partition_information" != "" ]]; then
+				array=("$json" "$partition_information")
+				partition_json=$(combine_json "${array[@]}")
 			else
-				partitionJson="$json"
+				partition_json="$json"
 			fi
 		fi
 	done < <(echo "$disklist")
 	
 	#finish up json
-	partitionJson="{\"partitions\":$partitionJson}"
-	echo $partitionJson
+	partition_json="{\"partitions\":$partition_json}"
+	echo $partition_json
 }
 
 get_smart_data() {
-	local smartDataJson
+	local smartdata_json
+	local smartdata_raw
 	currentdevice=$1
-	declare -A SmartData
+	declare -A smart_data
 	
 	#get smart data
-	smartdataraw=$(smartctl -i /dev/$currentdevice)
+	if [ $(check_if_installed "smartctl") -eq 1 ]; then
+		smartdata_raw=$(smartctl -i /dev/$currentdevice)
+	fi
 
 	#Check if there are any errors
-	if [[ $smartdataraw != *"Permission denied"* ]] || [[ $smartdataraw != *"failed"* ]]; then
-		SmartData["modelFamily"]=$(echo "$smartdataraw" | awk '/^Model Family:/' | cut -d":" -f2 | sed "s/^[ \t]*//")
-		SmartData["deviceModel"]=$(echo "$smartdataraw" | awk '/^Device Model:/' | cut -d":" -f2 | sed "s/^[ \t]*//")
-		SmartData["serialNumber"]=$(echo "$smartdataraw" | awk '/^Serial Number:/' | cut -d":" -f2 | sed "s/^[ \t]*//")
-		SmartData["firmwareVersion"]=$(echo "$smartdataraw" | awk '/^Firmware Version:/' | cut -d":" -f2 | sed "s/^[ \t]*//")
-		SmartData["sectorSize"]=$(echo "$smartdataraw" | awk '/^Sector Size:/' | cut -d":" -f2 | sed "s/^[ \t]*//")
-		SmartData["rotationRate"]=$(echo "$smartdataraw" | awk '/^Rotation Rate:/' | cut -d":" -f2 | sed "s/^[ \t]*//")
-		SmartData["temperature"]=$(hddtemp /dev/$currentdevice | awk 'NF>1{print $NF}')
-		smartDataJson="$(get_json "$(declare -p SmartData) smartData")"
-		echo "$smartDataJson"
+	if [[ $smartdata_raw != *"Permission denied"* ]] && [[ $smartdata_raw != *"failed"* ]] && [[ $smartdata_raw != "" ]]; then
+		smart_data["modelFamily"]=$(echo "$smartdata_raw" | awk '/^Model Family:/' | cut -d":" -f2 | sed "s/^[ \t]*//")
+		smart_data["deviceModel"]=$(echo "$smartdata_raw" | awk '/^Device Model:/' | cut -d":" -f2 | sed "s/^[ \t]*//")
+		smart_data["serialNumber"]=$(echo "$smartdata_raw" | awk '/^Serial Number:/' | cut -d":" -f2 | sed "s/^[ \t]*//")
+		smart_data["firmwareVersion"]=$(echo "$smartdata_raw" | awk '/^Firmware Version:/' | cut -d":" -f2 | sed "s/^[ \t]*//")
+		smart_data["sectorSize"]=$(echo "$smartdata_raw" | awk '/^Sector Size:/' | cut -d":" -f2 | sed "s/^[ \t]*//")
+		smart_data["rotationRate"]=$(echo "$smartdata_raw" | awk '/^Rotation Rate:/' | cut -d":" -f2 | sed "s/^[ \t]*//")
+		
+		if [ $(check_if_installed "hddtemp") -eq 1 ]; then
+			smart_data["temperature"]=$(hddtemp /dev/$currentdevice | awk 'NF>1{print $NF}')
+		fi
+		smartdata_json="$(get_json "$(declare -p smart_data) smartData")"
+		echo "$smartdata_json"
 	fi
 }
-
 
 # loop through disks connected to the device
 endjson="{"
