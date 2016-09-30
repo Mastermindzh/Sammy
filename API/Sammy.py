cdb34d999b458cd694393d4a075185772ed82cc6
jancoow <jancokock@gmail.com>
2016-10-01 11:47:18 +1300

    Reverted our not working sammy.py

--- API/Sammy.py
+++ API/Sammy.py
@@ -1,109 +1,73 @@
-import os.path
+from flask_cache import Cache
+from flask import Flask, render_template, request
+from flask_cors import CORS, cross_origin
+import os.path, time
+import meinheld
+
 import subprocess
 
-from flask import Flask
-from flask_cache import Cache
-
-# store cache values
-cache_values={'minute': 60,'hour': 3600,'day': 86400, 'week': 604800}
+cache_values = {'minute': 60, 'hour': 3600, 'day': 86400, 'week': 604800}
 
 # initialize app with caching
 app = Flask(__name__)
 cache = Cache(config={'CACHE_TYPE': 'simple'})
 cache.init_app(app)
-
+CORS(app)
 
 # Greeter page
 @app.route('/')
-@cache.cached(timeout=cache_values['week'])
+@cache.cached(timeout=cache_values['day'])
 def hello_world():
-    return '<h1>Hello and welcome to Sammy!</h1>'
+    return render_template('index.html', info=getInfo())
+
+# CPU info
+@app.route('/cpu/', defaults={"info": "Bash/CPU/getAll.sh"})
+@app.route('/cpu/<info>')
+def get_cpu_info(info=None):
+    try:
+        routes = {
+            "temp": "Bash/CPU/getTemp.sh",
+            "info": "Bash/CPU/getInfo.sh",
+            "load": "Bash/CPU/getLoad.sh",
+        }
+        route = routes[info]
+    except Exception:
+        route = info
+
+    return run_bash_file(route)
 
 
-# Test method
-@app.route('/pwd')
-def test():
-    process = subprocess.Popen(["pwd"], stdout=subprocess.PIPE)
+# Mem info
+@app.route('/mem/', defaults={"info": "Bash/MEM/getAll.sh"})
+@app.route('/mem/<info>')
+def get_mem_info(info=None):
+    try:
+        routes = {
+            "info": "Bash/MEM/getInfo.sh",
+            "load": "Bash/MEM/getLoad.sh",
+        }
+        route = routes[info]
+    except Exception:
+        route = info
+
+    return run_bash_file(route)
+
+
+# System info
+@app.route('/sysinfo/')
+def get_sys_info():
+    process = subprocess.Popen("./Bash/SYSTEM/getSystemInfo.sh", stdout=subprocess.PIPE)
     return process.communicate()[0]
 
 
-# All cpu information
-@app.route('/cpu')
-def get_cpu_all():
-    return run_bash_file("Bash/CPU/getAll.sh")
-
-
-# CPU temperature
-@app.route('/cpu/temp')
-def get_cpu_temp():
-    return run_bash_file("Bash/CPU/getTemp.sh")
-
-
-# CPU load
-@app.route('/cpu/load')
-def get_cpu_load():
-    return run_bash_file("Bash/CPU/getLoad.sh")
-
-
-# CPU info
-@app.route('/cpu/info')
-def get_cpu_info():
-    return run_bash_file("Bash/CPU/getInfo.s")
-
-
-# all memory info
-@app.route('/mem')
-def get_mem_all():
-    return run_bash_file("Bash/MEM/getAll.sh")
-
-
-# memory info
-@app.route('/mem/info')
-def get_mem_info():
-    return run_bash_file("Bash/MEM/getInfo.sh")
-
-
-# memory info
-@app.route('/mem/load')
-def get_mem_load():
-    return run_bash_file("Bash/MEM/getLoad.sh")
-
-
-@app.route('/sysinfo')
-def get_sys_info():
-    return run_bash_file("Bash/getSystemInfo.sh")
-
-
-@app.route('/disk')
+# Disk info
+@app.route('/disks/')
 def get_disk_info():
-    return run_bash_file("Bash/DISK/getDiskInfo.sh")
+    process = subprocess.Popen(["bash", "Bash/DISK/getAll.sh"], stdout=subprocess.PIPE)
+    return process.communicate()[0]
 
 
-@app.route('/greyhole')
-def get_disk_info():
-    return run_bash_file("Bash/GREYHOLE/getGreyholeInfo.sh")
-
-
-@app.route('/greyhole/io')
-def get_disk_info():
-    return run_bash_file("Bash/GREYHOLE/getIO.sh")
-
-
-@app.route('/greyhole/log')
-def get_disk_info():
-    return run_bash_file("Bash/GREYHOLE/getLog.sh")
-
-
-@app.route('/greyhole/queue')
-def get_disk_info():
-    return run_bash_file("Bash/GREYHOLE/getQueue.sh")
-
-
-@app.route('/greyhole/statistics')
-def get_disk_info():
-    return run_bash_file("Bash/GREYHOLE/getStatistics.sh")
-
-
+# custom methods
 def run_bash_file(filepath):
     """Execute a bash file and returns the output of the command.
     Arguments: Path to the bash file.
@@ -113,4 +77,15 @@ def run_bash_file(filepath):
         process = subprocess.Popen(["bash", filepath], stdout=subprocess.PIPE)
         return process.communicate()[0]
     else:
-        return '{"error":"Bash file not found"}'
+        return '{"Error":"no such route."}'
+
+
+
+def getInfo():
+    pwd = subprocess.Popen("pwd", stdout=subprocess.PIPE, )
+    dict = {'Host': request.host, 'Working directory': pwd.communicate(0)[0]}
+    return dict
+
+
+meinheld.listen(("0.0.0.0", 5000))
+meinheld.run(app)
