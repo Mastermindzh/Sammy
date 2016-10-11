e84f58c6dddbbc790b7ae694ffbd14d6a2250b52
Janco Kock <jancokock@gmail.com>
2016-10-12 10:44:37 +1300

    Added error if samba configuration file is not found

--- API/Bash/SAMBA/getConfig.sh
+++ API/Bash/SAMBA/getConfig.sh
@@ -10,45 +10,49 @@
 
 get_config(){
 	#Config path where we should read from
-	configpath="/home/janco/smb.conf"
+	configpath="/etc/samba/smb.conf"
 
-	#assocotive array which will hold all the informatoin
-	declare -A samba_config
+	if [ ! -f $configpath ]; then
+		echo '{"error":"Samba configuration file not found"}'
+	else
+		#assocotive array which will hold all the informatoin
+		declare -A samba_config
 
-	while read line;
-	do
-		if [[ $line == \[*] ]] 		#Section
-		then
-			#get the json of the last section if there was one
-			if [ ! -z "$section" -a "$section" != " " ] 
+		while read line;
+		do
+			if [[ $line == \[*] ]] 		#Section
 			then
-				json="$(get_json "$(declare -p share_info)" "$section")"				
-				#Combine this to the total json object, if the total json object is not empty
-				if [[ "$samba_config" != "" ]]; then
-					array=("$json" "$samba_config")
-					samba_config=$(combine_json "${array[@]}")
-				else
-					samba_config="$json"
+				#get the json of the last section if there was one
+				if [ ! -z "$section" -a "$section" != " " ] 
+				then
+					json="$(get_json "$(declare -p share_info)" "$section")"				
+					#Combine this to the total json object, if the total json object is not empty
+					if [[ "$samba_config" != "" ]]; then
+						array=("$json" "$samba_config")
+						samba_config=$(combine_json "${array[@]}")
+					else
+						samba_config="$json"
+					fi
+					unset share_info
 				fi
-				unset share_info
+				# declare an associative array
+				declare -A share_info
+				section=$line
+			elif [[ $line != \#* ]] && [[ $line != \;* ]] && [ ! -z "$line" -a "$line" != " " ] #Values
+			then
+				key=$(echo $line | cut -d"=" -f 1)
+				value=$(echo $line | cut -d"=" -f 2)
+				share_info[$key]=$value
 			fi
-			# declare an associative array
-			declare -A share_info
-			section=$line
-		elif [[ $line != \#* ]] && [[ $line != \;* ]] && [ ! -z "$line" -a "$line" != " " ] #Values
-		then
-			key=$(echo $line | cut -d"=" -f 1)
-			value=$(echo $line | cut -d"=" -f 2)
-			share_info[$key]=$value
-		fi
-	done < $configpath
+		done < $configpath
 
-	#finish up json
-	json="$(get_json "$(declare -p share_info)" $section)"	
-	array=("$json" "$samba_config")
-	samba_config=$(combine_json "${array[@]}")
-	samba_config="{\"shares\":$samba_config}"				
-	echo $samba_config
+		#finish up json
+		json="$(get_json "$(declare -p share_info)" $section)"	
+		array=("$json" "$samba_config")
+		samba_config=$(combine_json "${array[@]}")
+		samba_config="{\"shares\":$samba_config}"				
+		echo $samba_config
+	fi
 }
 
 get_config
